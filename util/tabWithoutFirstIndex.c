ed68f44dea267371f7c69a67c5cdf50f43ddae2e
Damien <damien.chevalerias@etu.univ-nantes.fr>
2018-03-19 23:33:30 +0100

    Delete util function
    La fonction qui récupérait tout un tableau sans le premier index peut être remplacer par une ligne :)

--- util/tabWithoutFirstIndex.c
+++
@@ -1,8 +0,0 @@
-char *tabWithoutFirstIndex(int length, char *tab[]){
-	length--;
-	char *tabWFI = malloc(sizeof(char) * length);
-	for (int i = 0; i < length; i++) {
-		tabWFI[i] = tab[i + 1];
-	}
-	return tabWFI;
-}
