fff1d8b819bb1031b0e384bbce13798f34552cb3
Bastien CHUPIN <bastien.chupin@etu.univ-nantes.fr>
2018-03-19 21:02:51 +0100

    Update call.c

--- call.c
+++ call.c
@@ -1,34 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int exeExists(char arg) {
-  
-}
-
-int fctExists() {
-  
-}
-
-int libExists() {
-  
-}
-
-
 int call(int argc, char *argv[]) {
-  char *cmd = argv[0];
-	if (exeExists(cmd)) {
-    char path[80];
+    char *cmd = argv[0];
+    
+    #ifdef identifier  
+        char path[80];
 		strcpy(path, cmd);
 		strcat(path, ".exe");
-  	execv(path,argv + 1);
-  }
-  else if (fctExists()) {
-    
-  }
-  else if (libExists()) {
-	    
-  }
-  else {
-    return -1;
-  }
+  	    execv(path,argv + 1);
+    #elif 
+
+    #else
+
+    #endif
+
 }
