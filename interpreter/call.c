a265df638ab14cf8396e0a70e1483d39055cc128
bastien <bastien.chupin@etu.univ-nantes.fr>
2018-04-23 21:26:45 +0200

    bastien
    return value de la fonction call

--- interpreter/call.c
+++ interpreter/call.c
@@ -7,15 +7,18 @@
 
 int call(int argc, char *argv[]) {
   char *cmd_name = argv[0];
+  int exit_value;
 #ifdef EXECUTABLE
   char path[80];
   strcpy(path, "cmd/bin/");
   strcat(path, cmd_name);
-  execv(path, argv);
+  exit_value = execv(path, argv);
+  if(exit_value==-1) return 0;
+  return 1;
 #elif INTEGRATED_FUNCTION
   cmd_t cmd = getCmd(cmd_name);
   if (cmd)
-    cmd(argc - 1, argv + 1);
+    cmd(argc - 1, argv + 1); //Ne retourne pas de valeur de reussite
 //TODO else si commande introuvable
 #elif LIBRARY
   char path[80];
