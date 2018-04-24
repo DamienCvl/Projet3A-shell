a1233412033d71de0a4bdb8ea1cb68a96d0bf774
Yves Le Palud <yves.le-palud@etu.univ-nantes.fr>
2018-04-24 15:54:35 +0200

    Implementation du parseur et de l'interpreteur

--- interpreter/interpreter.c
+++ interpreter/interpreter.c
@@ -2,78 +2,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "call.c"
+#include "call.h"
 
-char *readCmd();
-char **concCmd(char *cmd);
+#define TAILLE_MAX_ARGUMENT 100
+#define NBR_MAX_DE_COMMANDE 25
+#define BUFFER_SIZE 1024
+
+char *readInput();
 void directory();
+int parseInput(char *parsed[], char *input);
+int interpret(int argc, char *argv[]);
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     char *cmd;
     char **args;
-    int isLooping ; //savoir quand quitter
-
+    char parsedInput[NBR_MAX_DE_COMMANDE][TAILLE_MAX_ARGUMENT];
     do {
         directory();
-        printf("\n> ");
-        cmd = readCmd();
-        args = concCmd(cmd);
-        isLooping = call(args);
-
-        free(cmd); //à remplacer par une insertion dans une stack (flèche du haut)
-        free(args);
-    } while (isLooping);
-
+        char *input = readInput();
+        int nombreArgument = parseInput(parsedInput, input);
+        interpret(nombreArgument, parsedInput);
+    } while (1);
     return EXIT_SUCCESS;
 }
 
-void directory()
-{
-    char dir[1024];
-    getcwd(dir, sizeof(dir));
-    printf("\n> %s", dir);
+int interpret(int argc, char *argv[]){
+  for (int i = argc-1; i > 0; i--) {
+    if (strcmp("|", argv[i]) == 0) {
+      //redirectionCommandeVersCommande(i - 1, &argv[0], i + 1, &argv[i + 1]);
+    }
+    else if (strcmp(">", argv[i]) == 0) {
+      //redirectionCommandeVersFichierEnEcrasant(i - 1, &argv[0], i + 1, &argv[i + 1]);
+    }
+    else if (strcmp(">>", argv[i]) == 0) {
+      //redirectionCommandeVersFichierEnAjout(i - 1, &argv[0], i + 1, &argv[i + 1]);
+    }
+    else if (strcmp("<", argv[i]) == 0) {
+      //redirectionFichierVersCommande(i - 1, &argv[0], i + 1, &argv[i + 1]);
+    }
+    else if (strcmp("<<", argv[i]) == 0) {
+      //redirectionClavierVersCommande(i - 1, &argv[0], i + 1, &argv[i + 1]);
+    }
+    else {
+      call(argc, argv);
+    }
+  }
 }
 
-char *readCmd()
-{
+void directory() {
+    char dir[1024];
+    getcwd(dir, sizeof(dir));
+    printf("\n%s $", dir);
+}
+
+char *readInput() {
     char *line = NULL;
-    unsigned long bufsize = 0; //faut-il plus grand ?
+    unsigned int bufsize = BUFFER_SIZE;
     getline(&line, &bufsize, stdin);
     return line;
 }
 
-#define BUFFERSIZE_TOKEN 64
-#define STROK_DELIM " \n"
-char **concCmd(char *cmd)
-{
-    int tokBuff = BUFFERSIZE_TOKEN;
-    int  indice = 0;
-    char *tokens = malloc(tokBuff * sizeof(char));
-    char *token;
+int parseInput(char *parsed[], char *input) {
+  int currentIndex = 0, nombreArgument = 0;
+  char currentChar = input[currentIndex];
 
-    if (!tokens) {
-        fprintf(stderr, "Cannot Allocate Memory\n"); //Pas sur du message d'erreur d'allocation
-        exit(EXIT_FAILURE);
+  while (currentChar != '\n' || nombreArgument > NBR_MAX_DE_COMMANDE) {
+    if (currentChar == ' ') {
+      parsed[nombreArgument][currentIndex + 1] = '\0';
+      nombreArgument++;
+      currentIndex = 0;
+    } else {
+      parsed[nombreArgument][currentIndex] = currentChar;
     }
-
-    token = strtok(cmd, STROK_DELIM);
-    while (token != NULL) {
-        tokens[indice] = token;
-        indice++;
-
-        if (indice >= tokBuff) {
-            tokBuff += tokBuff;
-            tokens = realloc(tokens, tokBuff * sizeof(char*));
-            if (!tokens) {
-                fprintf(stderr, "Cannot Allocate Memory\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        token = strtok(NULL, STROK_DELIM);
-    }
-    tokens[indice] = NULL;
-    return tokens;
+  }
+  return nombreArgument;
 }
-
--- interpreter/test.c
+++
@@ -1,6 +0,0 @@
-#include "call.h"
-
-int main(int argc, char *argv[]) {
-  call(argc - 1, argv + 1);
-  return 0;
-}
