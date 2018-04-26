c96a8f9f22dd3c4adec47c6dfed2212885f8ff7f
Johan Sorette <johan.sorette@etu.univ-nantes.fr>
2018-04-26 18:25:39 +0200

    Fonctionnement des redirections lecture de fichier + lecture clavier

--- interpreter/redirection.c
+++ interpreter/redirection.c
@@ -9,10 +9,13 @@
 #include "redirection.h"
 
 #define BUFFER_SIZE 1024
+#define WAIT_CHILD 1
+#define DO_NOT_WAIT_CHILD 0
 
-int myPipe(int argcEntree, char *argvEntree[], int argcSortie, char *argvSortie[], int (*fctEntree)(int, char *[]), int (*fctSortie)(int, char *[])){
+int myPipe(int argcEntree, char *argvEntree[], int argcSortie, char *argvSortie[], int (*fctEntree)(int, char *[]), int (*fctSortie)(int, char *[]), int waitChild){
    int fd[2];
    pid_t pid_fils;
+   int status;
    pipe(fd);
 
    if ((pid_fils = fork()) == -1) {
@@ -27,17 +30,26 @@ int myPipe(int argcEntree, char *argvEntree[], int argcSortie, char *argvSortie[
    }
    close(fd[1]);
    dup2(fd[0],STDIN_FILENO);
+   if (waitChild) {
+     waitpid(pid_fils, &status, 0);
+   }
    return (*fctSortie)(argcSortie, argvSortie);
 }
 
 int redirigerVersFichier(char *filename, char *mode) {
   FILE *fichier = NULL;
   fichier = fopen(filename, mode);
-  int currentChar;
-  while (!feof(stdin)) {
-    currentChar = fgetc(stdin);
-    fputc(currentChar, fichier);
+
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  while ((read = getline(&line, &len, stdin)) != -1) {
+    fputs(line, fichier);
   }
+  if (line) {
+    free(line);
+  }
+
   fclose(fichier);
   return 0;
 }
@@ -59,43 +71,56 @@ int lireDepuisFichier(int argcEntree, char *argvEntree[]) {
   FILE *fichier = NULL;
   char *filename = argvEntree[0];
   fichier = fopen(filename, "r");
-  int currentChar;
-  while (!feof(fichier)) {
-    currentChar = fgetc(stdin);
-    printf("%c", currentChar);
+
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  while ((read = getline(&line, &len, fichier)) != -1) {
+    printf("%s", line);
   }
+  if (line){
+    free(line);
+  }
+
   fclose(fichier);
   return 0;
 }
 
 int lireDepuisClavier(int argcEntree, char *argvEntree[]) {
-  char *chaine = NULL;
   char *fin = argvEntree[0];
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  strcat(fin, "\n");
   do {
-    if (chaine != NULL)
-      printf("%s", chaine);
-    scanf("%s", chaine);
-  } while (strcmp(chaine, fin) != 0);
+    getline(&line, &len, stdin);
+    if (strcmp(line, fin) != 0) {
+      printf("%s", line);
+    }
+  } while (strcmp(line, fin) != 0);
   return 0;
 }
 
 // Interfaces de redirection
 int redirectionCommandeVersCommande(int argcEntree, char *argvEntree[], int argcSortie, char *argvSortie[]) {
-  return myPipe(argcEntree, argvEntree, argcSortie, argvSortie, interpret, interpret);
+  return myPipe(argcEntree, argvEntree, argcSortie, argvSortie, interpret, interpret, DO_NOT_WAIT_CHILD);
 }
 
 int redirectionCommandeVersFichierEnAjout(int argcEntree, char *argvEntree[], int argcSortie, char *argvSortie[]) {
-  return myPipe(argcEntree, argvEntree, argcSortie, argvSortie, interpret, redirigerVersFichierEnAjout);
+  return myPipe(argcEntree, argvEntree, argcSortie, argvSortie, interpret, redirigerVersFichierEnAjout, DO_NOT_WAIT_CHILD);
 }
 
 int redirectionCommandeVersFichierEnEcrasant(int argcEntree, char *argvEntree[], int argcSortie, char *argvSortie[]) {
-  return myPipe(argcEntree, argvEntree, argcSortie, argvSortie, interpret, redirigerVersFichierEnEcrasant);
+  return myPipe(argcEntree, argvEntree, argcSortie, argvSortie, interpret, redirigerVersFichierEnEcrasant, DO_NOT_WAIT_CHILD);
 }
 
 int redirectionFichierVersCommande(int argcEntree, char *argvEntree[], int argcSortie, char *argvSortie[]) {
-  return myPipe(argcEntree, argvEntree, argcSortie, argvSortie, lireDepuisFichier, interpret);
+  // Switch Entree/Sortie
+  return myPipe(argcSortie, argvSortie, argcEntree, argvEntree, lireDepuisFichier, interpret, DO_NOT_WAIT_CHILD);
 }
 
 int redirectionClavierVersCommande(int argcEntree, char *argvEntree[], int argcSortie, char *argvSortie[]) {
-  return myPipe(argcEntree, argvEntree, argcSortie, argvSortie, lireDepuisClavier, interpret);
+  // Switch Entree/Sortie
+  return myPipe(argcSortie, argvSortie, argcEntree, argvEntree, lireDepuisClavier, interpret, WAIT_CHILD);
 }
