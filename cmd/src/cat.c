9f3de4c7254a8876f558c1de8a0896d676d7b13e
Johan Sorette <johan.sorette@etu.univ-nantes.fr>
2018-04-26 18:26:08 +0200

    Correctif sur cat pour utiliser getline au lieu de getc

--- cmd/src/cat.c
+++ cmd/src/cat.c
@@ -25,10 +25,14 @@ int cat(int argc, char *argv[]) {
     file = stdin;
   }
 
-  while (!feof(file)) {
-    c = fgetc(file);
-    printf("%c", c);
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  while ((read = getline(&line, &len, file)) != -1) {
+    printf("%s", line);
   }
+  if (line)
+    free(line);
 
   fclose(file);
   return(0);
